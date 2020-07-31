ad03bfe750cd97692810ab84c46ad263d1aa66f4
AntonVoronezh <pstut@mail.ru>
2020-07-31 15:49:33 +0300

    init

--- src/framework/core/component.js
+++ src/framework/core/component.js
@@ -1,10 +1,17 @@
 export class Component {
-    constructor(config) {
-        this.template = config.template;
-        this.selector = config.selector;
-    }
+  constructor(config) {
+    this.template = config.template;
+    this.selector = config.selector;
+    this.el = null;
+  }
 
-    render(){
-        document.querySelector(this.selector).innerHTML = this.template;
+  render() {
+    this.el = document.querySelector(this.selector);
+
+    if (!this.el) {
+      throw new Error(`Component with selector ${this.selector} was not found`);
+    } else {
+        this.el.innerHTML = this.template;
     }
+  }
 }

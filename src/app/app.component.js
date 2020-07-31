a82efc6b9e8b9e8ad19fb1a269a176e8fc7fc680
AntonVoronezh <pstut@mail.ru>
2020-07-31 15:49:18 +0300

    init

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,6 +9,19 @@ class AppComponent extends WFMComponent {
 export const appComponent = new AppComponent({
   selector: "app-root",
   template: `
-    <div><h4>App Component work</h4></div>
+    <app-header></app-header>
+    <div class="row">
+      <div class="col s6 offset-s3" style="margin-top: 40px;">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">Главная страница</span>
+            <p>Пока нет текста.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Перейти на другую страницу</a>
+          </div>
+        </div>
+      </div>
+    </div>
     `,
 });
